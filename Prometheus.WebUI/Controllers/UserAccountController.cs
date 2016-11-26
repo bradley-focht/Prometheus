54114274caad81e34674b42800aa32f8a35e9449
Bradley <bradley_focht@myaccess.ca>
2016-11-25 21:10:42 -0600

    AD Login now works. Also made Sean a hamburger menu.

--- Prometheus.WebUI/Controllers/UserAccountController.cs
+++ Prometheus.WebUI/Controllers/UserAccountController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using Common.Dto;
+using Prometheus.WebUI.Helpers;
 using Prometheus.WebUI.Models.UserAccount;
 
 namespace Prometheus.WebUI.Controllers
@@ -18,24 +20,44 @@ namespace Prometheus.WebUI.Controllers
         /// <summary>
         /// Login Authorization and builds session cookie
         /// </summary>
-        /// <param name="uAccount"></param>
+        /// <param name="userLogin">Contains user credentials</param>
         /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Login(UserAccountModel uAccount)
+        public ActionResult Login(UserAccountModel userLogin)
         {
             if (!ModelState.IsValid)
                 return View("Index");
 
             //validate login, create session cookie
+            var um = new global::UserManager.UserManager();     /*this is weird, sean why did you name it like this? */
+            var user = (UserDto)um.Login(userLogin.Username, userLogin.Password);
+            if (user != null)
+            {
+                Session["Displayname"] = user.Name;
+                Session["Id"] = user.Id;
+                Session["Roles"] = user.Role;
 
-            return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["MessageType"] = WebMessageType.Failure;
+            TempData["Message"] = "Login failed. If you do not know your username or password then contact your IT Service Desk";
+
+            return RedirectToAction("Index");
         }
 
+        /// <summary>
+        /// Login as Guest
+        /// </summary>
+        /// <returns></returns>
         [ValidateAntiForgeryToken]
+        [HttpPost]
         public ActionResult LoginGuest()
         {
-            return RedirectToAction("Index", "Home");    
+            Session["DisplayName"] = "Guest";
+            Session["Id"] = 0;
+            return RedirectToAction("Index", "Home");
         }
 
         /// <summary>
