033461e62883e69bc76e706fda85229a756ce2e2
Sean Bocz <seanboczulak@gmail.com>
2016-12-16 11:17:57 -0600

    Added Business and Support Catalogs. Also added DI bindings for catalog functions and PortfolioService

---
+++ RequestService/CatalogController.cs
@@ -0,0 +1,58 @@
+﻿using Common.Dto;
+using Common.Enums;
+using DataService;
+using DataService.DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RequestService
+{
+	public class CatalogController : ICatalogController
+	{
+		private int _userId;
+
+		public CatalogController(int userId)
+		{
+			_userId = userId;
+		}
+
+		public IEnumerable<IServiceDto> BusinessCatalog
+		{
+			get
+			{
+				using (var context = new PrometheusContext())
+				{
+					var services = context.Services.ToList()
+						.Where(x => x.ServiceTypeRole == ServiceTypeRole.Business && x.LifecycleStatus.CatalogVisible == true);
+
+					foreach (var service in services)
+					{
+						yield return ManualMapper.MapServiceToDto(service);
+					}
+				}
+			}
+		}
+
+		//TODO: Add in role check once permissions are in place
+		public IEnumerable<IServiceDto> SupportCatalog
+		{
+			get
+			{
+				using (var context = new PrometheusContext())
+				{
+					var user = context.Users.Find(_userId);
+					if (user == null)
+						yield break;
+
+					var services = context.Services.ToList()
+						.Where(x => x.ServiceTypeRole == ServiceTypeRole.Supporting && x.LifecycleStatus.CatalogVisible == true);
+
+					foreach (var service in services)
+					{
+						yield return ManualMapper.MapServiceToDto(service);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ RequestService/ICatalogController.cs
@@ -0,0 +1,18 @@
+﻿using Common.Dto;
+using System.Collections.Generic;
+
+namespace RequestService
+{
+	public interface ICatalogController
+	{
+		/// <summary>
+		/// List of all publicly viewable services
+		/// </summary>
+		IEnumerable<IServiceDto> BusinessCatalog { get; }
+
+		/// <summary>
+		/// List of all viewable Supporting services
+		/// </summary>
+		IEnumerable<IServiceDto> SupportCatalog { get; }
+	}
+}
