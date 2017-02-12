f559aa40f25640e07fbeffefb0d48700f0d2f1d0
Sarah H <sarah.halkyard@gmail.com>
2017-02-12 10:36:45 -0600

    Adding REST API to project

---
+++ RESTAPI/Controllers/RequestController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RESTAPI.Models;
+
+namespace RESTAPI.Controllers
+{
+    public class RequestController : ApiController
+    {
+        // GET: api/Request
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Request/5
+        public Request Get(int id)
+        {
+            Request request = new Request();
+
+            request.Id = id;
+            request.Name = "";
+            request.Creator = "";
+            request.Approver = "";
+            return request;
+        }
+
+        // POST: api/Request
+        public void Post([FromBody]Request value)
+        {
+        }
+
+        // PUT: api/Request/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Request/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RESTAPI/Models/Request.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTAPI.Models
+{
+    public class Request
+    {
+        //ID of the Service Request
+        public long Id { get; set; }
+
+        //Name of the Service Request
+        public string Name { get; set; }
+
+        //Name of the user who submitted the Service Request
+        public string Creator { get; set; }
+
+        //Name of the user who is set as the Approver for the Service Request
+        public string Approver { get; set; }
+
+    }
+}
