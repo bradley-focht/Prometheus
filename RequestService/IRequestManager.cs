0644bccd9b43040652db8bdb26bd221a08ee5b97
Sean Boczulak <seanboczulak@gmail.com>
2017-02-20 14:08:54 -0800

    Added state checking to the Request Manager and created the interface for using it.

---
+++ RequestService/IRequestManager.cs
@@ -0,0 +1,55 @@
+﻿using Common.Dto;
+using Common.Enums;
+
+namespace RequestService
+{
+	public interface IRequestManager
+	{
+		/// <summary>
+		/// Changes the state of a service request to the state provided if the action is possible.
+		/// </summary>
+		/// <param name="userId">ID of user performing the state change</param>
+		/// <param name="requestId">ID of Service Request to change the state of</param>
+		/// <param name="state">State to change the Service Request to</param>
+		/// <param name="comments">Optional: Comments tied to the state change if applicable</param>
+		/// <returns>Service Request after state change is attempted</returns>
+		IServiceRequestDto ChangeRequestState(int userId, int requestId, ServiceRequestState state, string comments = null);
+
+		/// <summary>
+		/// Changes the state of a service request to Submitted if the action is possible.
+		/// </summary>
+		/// <param name="userId">ID of user Submitting the request</param>
+		/// <param name="requestId">ID of Service Request to Submit</param>
+		/// <param name="comments">Optional: Comments tied to the submission if applicable</param>
+		/// <returns>Service Request after Submition is attempted</returns>
+		IServiceRequestDto SubmitRequest(int userId, int requestId, string comments);
+
+		/// <summary>
+		/// Changes the state of a service request to Cancelled if the action is possible.
+		/// </summary>
+		/// <param name="userId">ID of user Cancelling the request</param>
+		/// <param name="requestId">ID of Service Request to Cancel</param>
+		/// <param name="comments">Optional: Comments tied to the submission if applicable</param>
+		/// <returns>Service Request after Cancellation is attempted</returns>
+		IServiceRequestDto CancelRequest(int userId, int requestId, string comments);
+
+		/// <summary>
+		/// Changes the state of a service request to the result of the Approval if the action is possible.
+		/// </summary>
+		/// <param name="userId">ID of user Approving the request</param>
+		/// <param name="requestId">ID of Service Request to Approve</param>
+		/// <param name="approval">Result of the approval transaction (approved or denied)</param>
+		/// <param name="comments">Optional: Comments tied to the Approval if applicable</param>
+		/// <returns>Service Request after Approval is attempted</returns>
+		IServiceRequestDto ApproveRequest(int userId, int requestId, ApprovalResult approval, string comments);
+
+		/// <summary>
+		/// Changes the state of a service request to Fulfilled if the action is possible.
+		/// </summary>
+		/// <param name="userId">ID of user Fulfilling the request</param>
+		/// <param name="requestId">ID of Service Request to Fulfill</param>
+		/// <param name="comments">Optional: Comments tied to the Fulfillment if applicable</param>
+		/// <returns>Service Request after Fulfillment is attempted</returns>
+		IServiceRequestDto FulfillRequest(int userId, int requestId, string comments);
+	}
+}
--- RequestService/RequestManager.cs
+++ RequestService/RequestManager.cs
@@ -7,9 +7,94 @@ using DataService.Models;
 
 namespace RequestService
 {
-	public class RequestManager
+	public class RequestManager : IRequestManager
 	{
 		public IServiceRequestDto ChangeRequestState(int userId, int requestId, ServiceRequestState state, string comments = null)
+		{
+			IServiceRequestDto request = RequestFromId(requestId);
+			
+			switch (state)
+			{
+				case ServiceRequestState.Incomplete:
+					throw new ServiceRequestStateException("Cannot change the state of a Service Request to Incomplete.");
+				case ServiceRequestState.Submitted:
+					return SubmitRequest(userId, requestId, comments);
+				case ServiceRequestState.Approved:
+				case ServiceRequestState.Denied:
+					ApprovalResult approval = state == ServiceRequestState.Approved ? ApprovalResult.Approved : ApprovalResult.Denied;
+					return ApproveRequest(userId, requestId, approval, comments);
+				case ServiceRequestState.Cancelled:
+					return CancelRequest(userId, requestId, comments);
+				case ServiceRequestState.Fulfilled:
+					return FulfillRequest(userId, requestId, comments);
+			}
+			return request;
+		}
+
+		public IServiceRequestDto SubmitRequest(int userId, int requestId, string comments)
+		{
+			IServiceRequestDto request = RequestFromId(requestId);
+			if (request.State != ServiceRequestState.Incomplete)
+			{
+				throw new ServiceRequestStateException(
+					   string.Format("Cannot change the state of a Service Request to \"{0}\". " +
+									 "Service Request is in the \"{1}\" state and must be in the " +
+									 "\"{2}\" state to perform this action.",
+									 ServiceRequestState.Submitted, request.State, ServiceRequestState.Incomplete));
+			}
+
+			return request;
+		}
+
+		public IServiceRequestDto CancelRequest(int userId, int requestId, string comments)
+		{
+			IServiceRequestDto request = RequestFromId(requestId);
+
+			if (request.State != ServiceRequestState.Incomplete && request.State != ServiceRequestState.Submitted)
+			{
+				throw new ServiceRequestStateException(
+					string.Format("Cannot change the state of a Service Request to \"{0}\". " +
+								  "Service Request is in the \"{1}\" state and must be in the " +
+								  "\"{2}\" or \"{3}\" state to perform this action.",
+								  ServiceRequestState.Cancelled, request.State, ServiceRequestState.Incomplete, ServiceRequestState.Submitted));
+			}
+
+			return request;
+		}
+
+		public IServiceRequestDto ApproveRequest(int userId, int requestId, ApprovalResult approval, string comments)
+		{
+			IServiceRequestDto request = RequestFromId(requestId);
+
+			if (request.State != ServiceRequestState.Submitted)
+			{
+				throw new ServiceRequestStateException(
+					   string.Format("Cannot change the state of a Service Request to \"{0}\". " +
+									 "Service Request is in the \"{1}\" state and must be in the " +
+									 "\"{2}\" state to perform this action.", 
+									 approval, request.State, ServiceRequestState.Submitted));
+			}
+				
+			return request;
+		}
+
+		public IServiceRequestDto FulfillRequest(int userId, int requestId, string comments)
+		{
+			IServiceRequestDto request = RequestFromId(requestId);
+
+			if (request.State != ServiceRequestState.Approved)
+			{
+				throw new ServiceRequestStateException(
+					string.Format("Cannot change the state of a Service Request to \"{0}\". " +
+								  "Service Request is in the \"{1}\" state and must be in the " +
+								  "\"{2}\" state to perform this action.",
+								  ServiceRequestState.Fulfilled, request.State, ServiceRequestState.Approved));
+			}
+			
+			return request;
+		}
+
+		private IServiceRequestDto RequestFromId(int requestId)
 		{
 			IServiceRequestDto request;
 			using (var context = new PrometheusContext())
@@ -17,39 +102,10 @@ namespace RequestService
 				request = ManualMapper.MapServiceRequestToDto(context.ServiceRequests.Find(requestId));
 			}
 
-			if(request == null)
+			if (request == null)
 				throw new EntityNotFoundException("", typeof(ServiceRequest), requestId);
 
-			switch (state)
-			{
-				case ServiceRequestState.Incomplete:
-					throw new ServiceRequestStateException("Cannot change the state of a Service Request to Incomplete.");
-				case ServiceRequestState.Submitted:
-					break;
-				case ServiceRequestState.Approved:
-				case ServiceRequestState.Denied:
-					break;
-				case ServiceRequestState.Cancelled:
-					break;
-				case ServiceRequestState.Fulfilled:
-					break;
-			}
 			return request;
-		}
-
-		public bool ApproveRequest(int userId, int requestId, ApprovalResult approval, string comments)
-		{
-			return false;
-		}
-
-		public bool CancelRequest(int userId, int requestId, string comments)
-		{
-			return false;
-		}
-
-		public bool FulfillRequest(int userId, int requestId, string comments)
-		{
-			return false;
 		}
 	}
 }
