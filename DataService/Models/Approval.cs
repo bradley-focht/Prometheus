834b61e4862bcf7788ccc2bf33701c76bc94ba07
Sean Boczulak <seanboczulak@gmail.com>
2017-02-21 15:55:12 -0800

    Added Approval and Approval DTO. Added Approval creation and state change in RequestManager.

---
+++ DataService/Models/Approval.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Common.Enums;
+
+namespace DataService.Models
+{
+	public class Approval : IApproval
+	{
+		//PK
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		//FK
+		public int ServiceRequestId { get; set; }
+
+		#region Fields
+		public int ApproverId { get; set; }
+		public int RequestorId { get; set; }
+		public ApprovalResult Result { get; set; }
+		public string Comments { get; set; }
+
+		public DateTime? DateCreated { get; set; }
+		public DateTime? DateUpdated { get; set; }
+		public int CreatedByUserId { get; set; }
+		public int UpdatedByUserId { get; set; }
+		#endregion
+
+		#region Navigation properties
+		public virtual ServiceRequest ServiceRequest{ get; set; }
+		#endregion
+	}
+}
---
+++ DataService/Models/IApproval.cs
@@ -0,0 +1,17 @@
+﻿using Common.Enums;
+
+namespace DataService.Models
+{
+	public interface IApproval: IUserCreatedEntity
+	{
+		int Id { get; set; }
+		int ServiceRequestId { get; set; }
+
+		int ApproverId { get; set; }
+		int RequestorId { get; set; }
+		ApprovalResult Result { get; set; }
+		string Comments { get; set; }
+
+		ServiceRequest ServiceRequest { get; set; }
+	}
+}
