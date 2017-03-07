786194ce715fdb2db702d522d46e7408b4b4811d
Sean <seanboczulak@gmail.com>
2017-03-07 14:04:20 -0600

    Added Department Controller

---
+++ UserManager/Controllers/IDepartmentController.cs
@@ -0,0 +1,25 @@
+using Common.Dto;
+using Common.Enums.Entities;
+
+namespace UserManager.Controllers
+{
+	public interface IDepartmentController
+	{
+		/// <summary>
+		/// Retrieve a single department
+		/// </summary>
+		/// <param name="performingUserId">user making the request</param>
+		/// <param name="departmentId">department to retrieve</param>
+		/// <returns></returns>
+		IDepartmentDto GetDepartment(int performingUserId, int departmentId);
+
+		/// <summary>
+		/// Modifies the Department in the database
+		/// </summary>
+		/// <param name="performingUserId">User ID for the user performing the modification</param>
+		/// <param name="departmentDto"></param>
+		/// <param name="modification">Type of modification to make</param>
+		/// <returns></returns>
+		IDepartmentDto ModifyDepartment(int performingUserId, IDepartmentDto departmentDto, EntityModification modification);
+	}
+}
