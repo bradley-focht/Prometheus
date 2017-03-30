b2db44e17df5216f8862bf94221816d3b9a119ef
Sean <seanboczulak@gmail.com>
2017-03-29 21:41:58 -0600

    Commit for Sean's sake removing OAuth stuff. Ignore

--- Prometheus.WebUI/Models/UserAccount/UserAccountModel.cs
+++ Prometheus.WebUI/Models/UserAccount/UserAccountModel.cs
@@ -5,24 +5,24 @@ namespace Prometheus.WebUI.Models.UserAccount
 	/// <summary>
 	/// Login credentials
 	/// </summary>
-    public class UserAccountModel
-    {
+	public class UserAccountModel
+	{
 		/// <summary>
 		/// username
 		/// </summary>
-        [Required(ErrorMessage = "Enter username")]
-        public string Username { get; set; }
+		[Required(ErrorMessage = "Enter username")]
+		public string Username { get; set; }
 
 		/// <summary>
 		/// User password
 		/// </summary>
-        [Required(ErrorMessage = "Enter password")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+		[Required(ErrorMessage = "Enter password")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 		/// <summary>
 		/// redirection after login if not entering from login
 		/// </summary>
-	    public string ReturnUrl { get; set; }
+		public string ReturnUrl { get; set; }
 
-    }
+	}
 }
