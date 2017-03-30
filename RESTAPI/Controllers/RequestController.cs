3791e47bbbd677a62ff867a8b41dacb96629c4a9
Sean <seanboczulak@gmail.com>
2017-03-30 06:25:23 -0600

    Implemented API Access permission behaviour in the REST API

--- RESTAPI/Controllers/RequestController.cs
+++ RESTAPI/Controllers/RequestController.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Authentication;
 using System.Web;
 using System.Web.Http;
 using Common.Dto;
 using Common.Enums.Entities;
+using Common.Enums.Permissions;
+using Common.Exceptions;
 using RequestService;
 using RequestService.Controllers;
 using RESTAPI.Models;
@@ -26,57 +29,95 @@ namespace RESTAPI.Controllers
 			_serviceRequestController = serviceRequestController;
 		}
 
-		// GET: api/Request
+		// GET: /Request
 		public IEnumerable<Request> Get()
 		{
 			var userId = Authenticate().Id;
+
+			IEnumerable<IServiceRequestDto<IServiceRequestOptionDto, IServiceRequestUserInputDto>> requests =
+				new List<IServiceRequestDto<IServiceRequestOptionDto, IServiceRequestUserInputDto>>();
+
 			//Need to determine what we want
-			var requests = _serviceRequestController.GetServiceRequests(userId);
+			if (_userManager.UserHasPermission(userId, ApiAccess.FullApiAccess))
+			{
+				requests = _serviceRequestController.GetServiceRequests(userId);
+			}
+			else if (_userManager.UserHasPermission(userId, ApiAccess.OnlyUsersRequests))
+			{
+				requests = _serviceRequestController.GetServiceRequests(userId).Where(x => x.ApproverUserId == userId || x.RequestedByUserId == userId);
+			}
+
 			return requests.Select(x => new Request(x));
 		}
 
-		// GET: api/Request/5
+		// GET: /Request/5
 		public Request Get(int id)
 		{
 			var userId = Authenticate().Id;
-			//Need to change userId
-			return new Request(_serviceRequestController.GetServiceRequest(userId, id));
+
+			var request = _serviceRequestController.GetServiceRequest(userId, id);
+
+			//Need to determine what we want
+			if (_userManager.UserHasPermission(userId, ApiAccess.FullApiAccess))
+			{
+				return new Request(request);
+			}
+			else if (_userManager.UserHasPermission(userId, ApiAccess.OnlyUsersRequests))
+			{
+				if (request.RequestedByUserId == userId || request.ApproverUserId == userId)
+					return new Request(request);
+			}
+			throw new PermissionException($"User is not able to access Service Request with ID {id}", userId, ApiAccess.OnlyUsersRequests);
 		}
 
-		// POST: api/Request
+		// POST: /Request
 		public void Post([FromBody]Request value)
 		{
-			//var um = new UserManagerService(
-			//	new PermissionController(),
-			//	new UserController(),
-			//	new RoleController(new PermissionController())
-			//);
-			//var srController = new ServiceRequestController(um);
-
-			////Need to change userId
-			//srController.ModifyServiceRequest(um.GuestId, value, EntityModification.Create);
+			throw new InvalidOperationException("The /Request API does not support creation of Service Requests");
 		}
 
-		// PUT: api/Request/5
+		// PUT: /Request/5
 		public Request Put(int id, [FromBody]string value)
 		{
 			var userId = Authenticate().Id;
+
 			//Convert the string to a Request
 			var request = Newtonsoft.Json.JsonConvert.DeserializeObject<Request>(value);
 
-			//Need to change userId
-			return new Request(_requestManager.ChangeRequestState(userId, id, request.State));
+			if (request.Id != id)
+				throw new InvalidOperationException("ID in the body of the PUT request must match the ID provided in the URI");
+
+			//Need to determine what we want
+			if (_userManager.UserHasPermission(userId, ApiAccess.FullApiAccess))
+			{
+				return new Request(_requestManager.ChangeRequestState(userId, id, request.State));
+			}
+			else if (_userManager.UserHasPermission(userId, ApiAccess.OnlyUsersRequests))
+			{
+				if (request.RequestedByUserId == userId || request.ApproverUserId == userId)
+					return new Request(_requestManager.ChangeRequestState(userId, id, request.State));
+			}
+			throw new PermissionException($"User is not able to access Service Request with ID {id}", userId, ApiAccess.OnlyUsersRequests);
 		}
 
-		// DELETE: api/Request/5
+		// DELETE: /Request/5
 		public void Delete(int id)
 		{
 			var userId = Authenticate().Id;
-			//Convert the string to a Request
+
 			var request = _serviceRequestController.GetServiceRequest(userId, id);
 
-			//Need to change userId
-			_serviceRequestController.ModifyServiceRequest(userId, request, EntityModification.Delete);
+			//Need to determine action based on permission we want
+			if (_userManager.UserHasPermission(userId, ApiAccess.FullApiAccess))
+			{
+				_serviceRequestController.ModifyServiceRequest(userId, request, EntityModification.Delete);
+			}
+			else if (_userManager.UserHasPermission(userId, ApiAccess.OnlyUsersRequests))
+			{
+				if (request.RequestedByUserId == userId || request.ApproverUserId == userId)
+					_serviceRequestController.ModifyServiceRequest(userId, request, EntityModification.Delete);
+			}
+			throw new PermissionException($"User is not able to access Service Request with ID {id}", userId, ApiAccess.OnlyUsersRequests);
 		}
 
 		private IUserDto Authenticate()
@@ -88,6 +129,9 @@ namespace RESTAPI.Controllers
 
 			if (user.Id == 0)
 				throw new AuthenticationException("Failed to login, please check username and password.");
+
+			if (!_userManager.UserHasPermission(user.Id, ApiAccess.OnlyUsersRequests))
+				throw new PermissionException("Cannot Access REST API", user.Id, ApiAccess.OnlyUsersRequests);
 
 			return user;
 		}
