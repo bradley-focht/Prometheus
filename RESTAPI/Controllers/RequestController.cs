02ca7a1ad2dfdfd25a71992ca4f2e7000f444063
Sean <seanboczulak@gmail.com>
2017-03-30 05:00:43 -0600

    Added Authentication for REST API. Simply grab headers and pull Username and Password headers

--- RESTAPI/Controllers/RequestController.cs
+++ RESTAPI/Controllers/RequestController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
+using System.Web;
 using System.Web.Http;
+using Common.Dto;
 using Common.Enums.Entities;
 using RequestService;
 using RequestService.Controllers;
@@ -26,16 +29,18 @@ namespace RESTAPI.Controllers
 		// GET: api/Request
 		public IEnumerable<Request> Get()
 		{
+			var userId = Authenticate().Id;
 			//Need to determine what we want
-			var requests = _serviceRequestController.GetServiceRequests(_userManager.GuestId);
+			var requests = _serviceRequestController.GetServiceRequests(userId);
 			return requests.Select(x => new Request(x));
 		}
 
 		// GET: api/Request/5
 		public Request Get(int id)
 		{
+			var userId = Authenticate().Id;
 			//Need to change userId
-			return new Request(_serviceRequestController.GetServiceRequest(_userManager.GuestId, id));
+			return new Request(_serviceRequestController.GetServiceRequest(userId, id));
 		}
 
 		// POST: api/Request
@@ -55,21 +60,36 @@ namespace RESTAPI.Controllers
 		// PUT: api/Request/5
 		public Request Put(int id, [FromBody]string value)
 		{
+			var userId = Authenticate().Id;
 			//Convert the string to a Request
 			var request = Newtonsoft.Json.JsonConvert.DeserializeObject<Request>(value);
 
 			//Need to change userId
-			return new Request(_requestManager.ChangeRequestState(2, id, request.State));
+			return new Request(_requestManager.ChangeRequestState(userId, id, request.State));
 		}
 
 		// DELETE: api/Request/5
 		public void Delete(int id)
 		{
+			var userId = Authenticate().Id;
 			//Convert the string to a Request
-			var request = _serviceRequestController.GetServiceRequest(_userManager.GuestId, id);
+			var request = _serviceRequestController.GetServiceRequest(userId, id);
 
 			//Need to change userId
-			_serviceRequestController.ModifyServiceRequest(_userManager.GuestId, request, EntityModification.Delete);
+			_serviceRequestController.ModifyServiceRequest(userId, request, EntityModification.Delete);
+		}
+
+		private IUserDto Authenticate()
+		{
+			var headers = HttpContext.Current.Request.Headers;
+			var username = headers["Username"];
+			var password = headers["Password"];
+			var user = _userManager.Login(username, password);
+
+			if (user.Id == 0)
+				throw new AuthenticationException("Failed to login, please check username and password.");
+
+			return user;
 		}
 	}
 }
