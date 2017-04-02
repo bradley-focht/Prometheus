fbeef5c2c7339062f249cc97865811a0a400801f
Sean <seanboczulak@gmail.com>
2017-04-01 18:20:04 -0600

    Enum comments

--- ServiceFulfillmentEngineWebJob/Api/Models/Enums/ServiceRequestState.cs
+++ ServiceFulfillmentEngineWebJob/Api/Models/Enums/ServiceRequestState.cs
@@ -5,11 +5,34 @@
 	/// </summary>
 	public enum ServiceRequestState
 	{
+		/// <summary>
+		/// Service Request has not yet been submitted, but a Service Package has been selected
+		/// </summary>
 		Incomplete,
+
+		/// <summary>
+		/// Service Request is complete and has been submitted for approval
+		/// </summary>
 		Submitted,
-		Approved,
+
+		/// <summary>
+		/// The Requestor of the Service Request has cancelled the Service Request before it could be approved or denied
+		/// </summary>
 		Cancelled,
+
+		/// <summary>
+		/// Approver has approved the Service Request
+		/// </summary>
+		Approved,
+
+		/// <summary>
+		/// Approver has denied the Service Request
+		/// </summary>
 		Denied,
+
+		/// <summary>
+		/// Service Request is Fulfilled
+		/// </summary>
 		Fulfilled
 	}
 }
